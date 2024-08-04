e66972065b9fa0f657bb5281a1c2e3d6d8aa1987
Lucas Abraao <Labraao957@gmail.com>
2024-08-04 18:53:04 -0300

    Criando a página de Login / Criar conta

--- routes.js
+++ routes.js
@@ -1,33 +1,13 @@
 const express = require("express");
 const route = express.Router();
+
 const homeController = require("./src/controllers/homeController");
-const contatoController = require("./src/controllers/contatoController");
+const loginController = require("./src/controllers/loginController");
 
-function meuMiddleware(req, res, next) {
-  req.session = { nome: "Luiz", sobrenome: "Miranda" };
-  console.log();
-  console.log("Passei no seu middleware.");
-  console.log();
-  next();
-}
+// Rotas da home
+route.get("/", homeController.index);
 
-//Rotas da Home
-/*route.get(
-  "/",
-  meuMiddleware,
-  homeController.paginaInicial,
-  function (req, res, next) {
-    console.log();
-    console.log("Ainda estou aqui...");
-    console.log(
-      `'ultimo Middleware' Olha o que tem na req.session.nome ${req.session.nome}`
-    );
-  }
-);*/
-route.get("/", homeController.paginaInicial);
-route.post("/", homeController.trataPost);
-
-// Rotas de contato
-route.get("/contato", contatoController.paginaInicial);
+// Rotas de login
+route.get("/login/index", loginController.index);
 
 module.exports = route;
